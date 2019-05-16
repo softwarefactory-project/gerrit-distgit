5e6850e6eb894bd6bda7ceddc2f8b34c492f0406
Matthieu Huin <mhuin@redhat.com>
2019-05-16 10:48:29 +0200

    Fix pre-build, add oauth plugin, add CI
    * Install the right version of bazel for branch 2.14
    * Patch gerrit code to fix bazel-related errors
    * Download plugins artifacts from gerrit-ci instead of building them
    * add gerrit oauth plugin
    * add CI playbooks
    
    TG-2731
    
    Change-Id: Ie9e2f75decad09a05f43e08a925f7b7ebbc44425

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -ex
 
 VERSION=2.14.7
+RELEASE=$(grep -e '^Release:' gerrit.spec  | awk '{print $2}' | grep -o '[[:digit:]]\+')
 PLUGIN_BRANCH=stable-2.14
 MYSQL_CONNECTOR_BUNDLE=5.1.41
-PLUGINS="avatars-gravatar delete-project reviewers-by-blame"
+PLUGINS="avatars-gravatar reviewers-by-blame oauth delete-project"
 
 function install_bazel() {
-    sudo yum install -y https://copr-be.cloud.fedoraproject.org/results/vbatts/bazel/epel-7-x86_64/00725354-bazel/bazel-0.11.1-1.el7.centos.x86_64.rpm
+    # Always pick the minimal bzl version for this particular gerrit version
+    curl -L --output /tmp/bzl_install.sh https://github.com/bazelbuild/bazel/releases/download/0.14.0/bazel-0.14.0-installer-linux-x86_64.sh
+    chmod +x /tmp/bzl_install.sh
+    sudo /tmp/bzl_install.sh
 }
 
 function fetch_gerrit_sources() {
@@ -24,19 +28,20 @@ function fetch_dependencies() {
 }
 
 function fetch_plugins() {
-    pushd plugins
+    mkdir -p gf_plugins
     for plugin in ${PLUGINS}; do
-        [ -d ${plugin} ] || git clone https://gerrit.googlesource.com/plugins/${plugin}
-        pushd ${plugin}
-            # Do not fail here because some plugin doesn't have stable branch
-            git checkout ${PLUGIN_BRANCH} || true
-        popd
+       echo "Downloading plugin ${plugin}..."
+       if [ "$plugin" == "avatars-gravatar" ]; then
+           prefix="master-"
+       else
+           prefix=""
+       fi
+       curl -o gf_plugins/${plugin}.jar -L https://gerrit-ci.gerritforge.com/view/Plugins-${PLUGIN_BRANCH}/job/plugin-${plugin}-bazel-${prefix}${PLUGIN_BRANCH}/lastSuccessfulBuild/artifact/bazel-bin/plugins/${plugin}/${plugin}.jar
     done
-    popd
 }
 
-function fix_var_lib_usage() {
-    git am -3 ../0001-QuickFix-read-only-usr-lib-dir.patch ../0001-Add-InitSFUser-method.patch
+function apply_patches() {
+    git am -3 ../0001-QuickFix-read-only-usr-lib-dir.patch ../0001-Add-InitSFUser-method.patch ../0001-Fix-some-bazel-related-problems.patch ../0001-Fix-obsolete-bower-repository.patch
     TAG=v${VERSION}-sf
     git tag -d ${TAG} || true
     git tag -a -m ${TAG} ${TAG}
@@ -44,20 +49,16 @@ function fix_var_lib_usage() {
 
 # Main functions
 function gerrit_prepare() {
-    type bazel || install_bazel
+    type /usr/local/bin/bazel || install_bazel
     [ -d gerrit ] || fetch_gerrit_sources
 }
 
 function gerrit_build() {
     pushd gerrit
-    fix_var_lib_usage
+    apply_patches
     # First build may fail because of incorrect bazel version detection, fix by adding 'return True' in
     # vim /home/centos/.cache/bazel/_bazel_centos/0a7817c27ccc8fb2acf95e4072130a42/external/io_bazel_rules_closure/closure/repositories.bzl +164
-    [ -f bazel-bin/release.war ] || bazel build release
-    fetch_plugins
-    for plugin in ${PLUGINS}; do
-        bazel build plugins/${plugin}:${plugin}.jar
-    done
+    [ -f bazel-bin/release.war ] || /usr/local/bin/bazel build --jobs=1 release
     popd
 }
 
@@ -65,14 +66,17 @@ function gerrit_install() {
     mkdir -p artifacts/lib artifacts/plugins
     cp -f gerrit/bazel-bin/release.war artifacts/
     cp -f gerrit/bazel-genfiles/plugins/*/*.jar artifacts/plugins/
+    cp -f gf_plugins/*.jar artifacts/plugins/
     pushd artifacts
     fetch_dependencies
+    echo "Preparing archive..."
     find ./ -type f -exec chmod 644 {} \;
-    tar czf ../gerrit-${VERSION}.tar.gz *
+    tar czf ../gerrit-${VERSION}-${RELEASE}.el7.tar.gz *
     popd
 }
 
-rm -f gerrit-${VERSION}.tar.gz
+rm -f gerrit-*.tar.gz
 gerrit_prepare
 gerrit_build
+fetch_plugins
 gerrit_install
